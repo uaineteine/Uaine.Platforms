ca35b09f7ee0be3eaa56abf4b548f0a6b9741cb6
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-03-07 12:46:42 +1100

    Add project files.

---
+++ Platforms/platforms.cs
@@ -0,0 +1,14 @@
+﻿namespace Uaine.Platforms
+{
+    public static class platforms
+    {
+        public const int windowsdesktop = 0;
+        public const int android = 1;
+        public const int windowsphone = 2;
+        public const int linux = 3;
+        public const int mac = 4;
+        public const int ios = 5;
+        public const int rasbian = 6;
+        public const int chromeOS = 7;
+    }
+}
