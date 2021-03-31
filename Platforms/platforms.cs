4f5b5c0ef80a1859adf33b9d49d3c924c6d3addc
Daniel Robert <Daniel.Robert.S-S@hotmail.com>
2021-03-31 16:05:18 +1100

    platforms update with new platform ID inbuilt

--- Platforms/platforms.cs
+++ Platforms/platforms.cs
@@ -5,10 +5,46 @@
         public const int windowsdesktop = 0;
         public const int android = 1;
         public const int windowsphone = 2;
-        public const int linux = 3;
+        public const int linuxdesktop = 3;
         public const int mac = 4;
         public const int ios = 5;
         public const int rasbian = 6;
         public const int chromeOS = 7;
+
+        public static int currentPlatform = windowsdesktop;
+        public static void setPlatform(int platID)
+        {
+            currentPlatform = platID;
+        }
+
+        public static bool isDesktop()
+        {
+            switch(currentPlatform)
+            {
+                case windowsdesktop:
+                case linuxdesktop:
+                case mac:
+                case rasbian:
+                case chromeOS:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
+
+        public static bool isPhoneOS()
+        {
+            switch (currentPlatform)
+            {
+                case windowsphone:
+                case ios:
+                case android:
+                    return true;
+
+                default:
+                    return false;
+            }
+        }
     }
 }
